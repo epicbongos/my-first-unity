9521447a71252244cdd9f0bcfea5c3604423abea
Reza Arya <rezaarya511@gmail.com>
2024-01-11 07:45:43 +0700

    make offset into reusable variable

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
- transform.position = player.transform.position + new Vector3(0, 5, -7);       
+        transform.position = player.transform.position + offset;
     }
 }
