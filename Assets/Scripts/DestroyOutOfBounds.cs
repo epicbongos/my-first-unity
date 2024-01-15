d6e2c76d0494e117c6e51a44b304df6adf6a8df2
Reza Arya <rezaarya511@gmail.com>
2024-01-15 08:19:50 +0700

    feat: :sparkles: add debug game over text

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,12 +16,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If Object goes past the player, show gameover text on the bottom left corner of the window
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
