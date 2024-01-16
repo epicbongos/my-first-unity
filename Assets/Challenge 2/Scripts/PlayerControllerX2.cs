38441e1276949e84807edc9b1f5d893d7aa03c41
Reza Arya <rezaarya511@gmail.com>
2024-01-16 08:56:49 +0700

    add cooldown for spawning dogs

--- Assets/Challenge 2/Scripts/PlayerControllerX2.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX2.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerControllerX2 : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float spawnCooldownTime = 2.0f;
 
     // Update is called once per frame
     void Update()
     {
+        spawnCooldownTime -= Time.deltaTime;
+
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && spawnCooldownTime < 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spawnCooldownTime = 1.0f;
         }
     }
 }
