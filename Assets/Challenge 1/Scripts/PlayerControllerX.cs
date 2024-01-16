db62c6a043f5ca36815044324f4e37751ab7c571
Reza Arya <rezaarya511@gmail.com>
2024-01-17 00:07:52 +0700

    fixing speed, rotation, rigidbody freeze, drag

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed = 19f;
+    public float speed = 15f;
     public float rotationSpeed = 40f;
     public float verticalInput;
     public float horizontalInput;
@@ -23,10 +23,10 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * horizontalInput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
 
 
     }
